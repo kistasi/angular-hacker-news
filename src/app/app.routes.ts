b4793cdac2f520b202830e21cf8b873e79ef7aa8
Marton Tasnadi <marton.tasnadi@gmail.com>
2023-12-18 12:42:42 +0100

    Create Stories component and set as frontpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StoriesComponent } from './stories/stories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StoriesComponent },
+];
