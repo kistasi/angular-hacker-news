8e18e96eaf059c1179487e5dc1b25de45dcc4236
Marton Tasnadi <marton.tasnadi@gmail.com>
2023-12-25 17:23:17 +0100

    Add tests for RelativeTimePipe

--- src/app/pipes/relative-time/relative-time.pipe.spec.ts
+++ src/app/pipes/relative-time/relative-time.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { RelativeTimePipe } from './relative-time.pipe';
 
 describe('RelativeTimePipe', () => {
+  let pipe: RelativeTimePipe;
+
+  beforeEach(() => {
+    pipe = new RelativeTimePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new RelativeTimePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should format timestamp correctly', () => {
+    const now = Date.now();
+    const oneHourAgo = now - 3600000; // 1 hour in milliseconds
+    expect(pipe.transform(oneHourAgo / 1000)).toEqual('about 1 hour ago');
+  });
+
+  it('should handle future timestamp correctly', () => {
+    const futureTime = Date.now() + 3600000; // 1 hour in the future
+    expect(pipe.transform(futureTime / 1000)).toEqual('in about 1 hour');
+  });
+
+  it('should handle current timestamp', () => {
+    const now = Date.now();
+    expect(pipe.transform(now / 1000)).toEqual('less than a minute ago');
   });
 });
